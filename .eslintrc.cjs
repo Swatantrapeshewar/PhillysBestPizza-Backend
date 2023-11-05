f447d0a351ea4351f6eeea27443e118496fdfd90
Swatantrapeshewar <swatantrapeshewar@gmail.com>
2023-11-05 18:57:12 +0530

    feat: typed-linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,17 @@
 /* eslint-env node */
 module.exports = {
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-type-checked',
+	],
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
 	root: true,
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		project: ['./tsconfig.json'],
+	},
 	// rules: {
 	//   "@typescript-eslint/no-unused-vars": "off",
 	// }
