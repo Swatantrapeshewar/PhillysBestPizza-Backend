fb1d9110ba90adbd5925290a19979d3f3f739af8
Swatantrapeshewar <swatantrapeshewar@gmail.com>
2023-11-20 23:40:33 +0530

    feat: pm2-configurations

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+      {
+        name: 'phillysbestpizzaBackend', // specify your app name
+        script: 'npm',
+        args: 'run start',
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        env: {
+          NODE_ENV: 'production',
+        },
+      },
+    ],
+  };
+  
