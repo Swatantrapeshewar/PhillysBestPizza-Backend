7d54bf6a15f42962353d73042d1a4d66cf4bced4
Swatantrapeshewar <swatantrapeshewar@gmail.com>
2023-11-23 22:07:25 +0530

    style: format

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,15 @@
 module.exports = {
-    apps: [
-      {
-        name: 'phillysbestpizzaBackend', // specify your app name
-        script: 'npm',
-        args: 'run start',
-        autorestart: true,
-        watch: false,
-        max_memory_restart: '1G',
-        env: {
-          NODE_ENV: 'production',
-        },
-      },
-    ],
-  };
-  
+	apps: [
+		{
+			name: 'phillysbestpizzaBackend', // specify your app name
+			script: 'npm',
+			args: 'run start',
+			autorestart: true,
+			watch: false,
+			max_memory_restart: '1G',
+			env: {
+				NODE_ENV: 'production',
+			},
+		},
+	],
+};
